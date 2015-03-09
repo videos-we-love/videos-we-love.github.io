368467bb94c1f773dba176e067bcdbd10ae4a04d
Erik Pragt <erik.pragt@jworks.nl>
2015-03-09 16:41:22 +0100

    Redesigned the UI.

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,39 @@
-angular.module('app', [])
-    .controller("MainController", function($http) {
+angular.module('app', ['ui.bootstrap'])
+    .controller("MainController", function ($scope, $http) {
         var vm = this;
         vm.videos = [];
+        $scope.order = 'dateAdded';
 
         $http.get('data/videos.json').
             success(function (data) {
                 console.log(data);
                 vm.videos = data.videos;
             });
+
+        //$scope.setOrder = function (order) {
+        //    $scope.order = order;
+        //};
     })
-    .filter('trustAsResourceUrl', ['$sce', function($sce) {
-        return function(val) {
+    .controller('RatingDemoCtrl', function ($scope) {
+        $scope.rate = 7;
+        $scope.max = 10;
+        $scope.isReadonly = true;
+
+        $scope.hoveringOver = function(value) {
+            $scope.overStar = value;
+            $scope.percent = 100 * (value / $scope.max);
+        };
+
+        $scope.ratingStates = [
+            {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+            {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+            {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+            {stateOn: 'glyphicon-heart'},
+            {stateOff: 'glyphicon-off'}
+        ];
+    })
+    .filter('trustAsResourceUrl', ['$sce', function ($sce) {
+        return function (val) {
             return $sce.trustAsResourceUrl(val);
         };
     }]);
