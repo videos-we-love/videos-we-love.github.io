f997d5f89bd4246982c278fb6d7a4cbc3ad6f4f9
Willem Dekker <wdekker@gmail.com>
2015-04-01 23:12:49 +0200

    initial docker file for elastic search

---
+++ docker/Load.js
@@ -0,0 +1,27 @@
+var fs = require("fs")
+var http = require("http")
+
+fs.readFile('../data/videos.json', function (err, data) {
+  if (err) throw err;
+  var videos = JSON.parse(data).videos
+  videos.map(function(video) {
+	  storeVideo(video)
+  });
+});
+
+function storeVideo(video) {
+	var options = {
+	  hostname: 'localhost',
+	  port: 9200,
+	  path: '/videoswelove/videos',
+	  method: 'POST',
+	};
+
+	var request = http.request(options, function(res) {
+		console.log('STATUS: ' + res.statusCode)
+		console.log("video" + video + " added");
+		res.pipe(process.stdout)
+	})
+	request.write(JSON.stringify(video))
+	request.end()	
+}
